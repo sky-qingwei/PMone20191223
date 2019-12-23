b60b1adc8f318e9336ff82a402b9b790539c069a
qingwei <shuaiwei0323@163.com>
2019-12-23 17:59:43 +0800

    PM初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 日志的详细与否-生产环境  开发环境
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+// main,js入口文件的职责:1.做全局范围内的依赖和配置 2.创建一个根VUE实例
